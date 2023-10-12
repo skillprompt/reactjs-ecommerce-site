31811f5367139eabfd984cdc84ac080d13bac243
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2023-10-12 13:07:19 +0545

    create login form

--- src/pages/login-page.tsx
+++ src/pages/login-page.tsx
@@ -6,7 +6,25 @@ export function LoginPage() {
       <nav className={styles.nav}>
         <h1>React S-com Site</h1>
       </nav>
-      <div>form</div>
+      <section className={styles.formSection}>
+        <h2>Login Form</h2>
+
+        <div className={styles.formContainer}>
+          <form>
+            <div>
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" />
+            </div>
+            <div>
+              <button type="button">Login</button>
+            </div>
+          </form>
+        </div>
+      </section>
     </div>
   );
 }
