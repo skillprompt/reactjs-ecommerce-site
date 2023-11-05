4b5ee5a136f5deb231154beb9bced2e1a66a5f33
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2023-11-05 13:03:27 +0545

    protect the dashboard page

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,9 +1,20 @@
 import { BaseLayout } from "../components/base-layout";
+import { useAuth } from "../store/authentication";
 
 export function DashboardPage() {
+  const auth = useAuth();
+
+  if (auth.isLoggedIn) {
+    return (
+      <BaseLayout>
+        <div>i am dashboard</div>
+      </BaseLayout>
+    );
+  }
+
   return (
     <BaseLayout>
-      <div>i am dashboard</div>
+      <p>You are not logged in. Please login to see the dashboard.</p>
     </BaseLayout>
   );
 }
