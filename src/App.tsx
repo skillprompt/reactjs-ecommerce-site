8ce0cd238b37f744a0da8f7fdad7735ed48f4331
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2023-11-05 12:26:40 +0545

    use react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 import { LoginPage } from "./pages/login-page";
 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h2>React S-com Site</h2>
+        <ul>
+          <li>
+            <Link to={`/login`}>Login</Link>
+          </li>
+          <li>
+            <Link to={`/about-us`}>About Us</Link>
+          </li>
+        </ul>
+      </div>
+    ),
+  },
+  {
+    path: "about-us",
+    element: <p>I am about us page</p>,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+]);
+
 function App() {
-  return (
-    <main>
-      <LoginPage />
-    </main>
-  );
+  return <RouterProvider router={routes}></RouterProvider>;
 }
 
 export default App;
