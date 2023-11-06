43461ec2a1a3c1e8bd1737044d57d0441c24637a
baijanaththaru-skillprompt <baijanaththaru@skillprompt.com>
2023-11-06 13:08:46 +0545

    create modal in about us page

--- src/pages/about-us.tsx
+++ src/pages/about-us.tsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import { BaseLayout } from "../components/base-layout";
+import { Modal } from "../components/modal";
 
 export function AboutUsPage() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleOpenModal = () => {
+    setIsOpen(true);
+  };
+
   return (
     <BaseLayout>
+      <Modal
+        isOpen={isOpen}
+        title={"My new modal"}
+        onCloseModal={handleCloseModal}
+      >
+        <p> i am new modal</p>
+      </Modal>
       <h3>I am about us page</h3>
+      <button type="button" onClick={handleOpenModal}>
+        Open
+      </button>
     </BaseLayout>
   );
 }
